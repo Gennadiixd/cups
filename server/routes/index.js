2d135976ce0009b9adb835d4fe5e997720f76955
Gennadiixd <gennadiixd@gmail.com>
2019-05-01 20:19:19 +0300

    added key file on server side

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,4 +6,13 @@ router.get('/test', function(req, res, next) {
   res.send('connected');
 });
 
+
+router.get('/key', function(req, res, next) {  
+  const fs = require('fs');
+  let fileUrl = './APIkey.txt';
+  let fromFile =  fs.readFileSync(fileUrl, "UTF-8",);
+  res.send(fromFile);
+});
+
+
 module.exports = router;
