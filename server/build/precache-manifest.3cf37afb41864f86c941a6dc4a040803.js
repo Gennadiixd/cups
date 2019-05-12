e40bd3279a587456acf810b7a840d0fc9fa86348
Yan Ovsyannikov <ianovsiannikov@gmail.com>
2019-05-12 19:50:22 +0300

    build

---
+++ server/build/precache-manifest.3cf37afb41864f86c941a6dc4a040803.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d8b96e20e1cfa24398fae18fb068fac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fdb582564ad5b4d72bfe",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "f209182bc38baf44460f",
+    "url": "/static/css/main.08bed61e.chunk.css"
+  },
+  {
+    "revision": "fdb582564ad5b4d72bfe",
+    "url": "/static/js/2.dd4ed562.chunk.js"
+  },
+  {
+    "revision": "f209182bc38baf44460f",
+    "url": "/static/js/main.90a2b249.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
