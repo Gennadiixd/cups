7f3376fb2b5f9040d7fec2f6f3a7e0d6745c1cb5
Gennadii <gennadiixd@gmail.com>
2019-05-12 22:17:46 +0200

    added default directory for files on server side

--- server/upload.js
+++ server/upload.js
@@ -1,10 +1,14 @@
 const IncomingForm = require("formidable").IncomingForm;
 
 module.exports = function upload(req, res) {
-  var form = new IncomingForm();
+  var form = new IncomingForm({
+    uploadDir: './upload' , 
+    keepExtensions: true
+  });
 
   form.on("file", (field, file) => {
-      console.log(file.path)
+    console.log(file.path)
+    console.log(file.name)
     // Do something with the file
     // e.g. save it to the database
     // you can access it using file.path
